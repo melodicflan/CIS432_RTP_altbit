63f5452fb0937686d8b906fb194995e5aa0bd712
Melody Li <melodyli.sy@gmail.com>
2013-11-06 21:39:52 -0800

    finished all methods. b sends back fresh packet

--- prog2.c
+++ prog2.c
@@ -15,8 +15,11 @@
      (although some can be lost).
  **********************************************************************/
 
-#define BIDIRECTIONAL 0    /* change to 1 if you're doing extra credit */
-/* and write a routine called B_output */
+#define BIDIRECTIONAL 0    /* change to 1 if you're doing extra credit and write a routine called B_output */
+
+//moved declarations 
+float simtime = 0.000; /* simulates "real" time */
+int TRACE = 1; /* for my debugging */
 
 /* a "msg" is the data unit passed from layer 5 (teachers code) to layer  */
 /* 4 (students' code).  It contains the data (characters) to be delivered */
@@ -38,7 +41,9 @@ struct pkt {
 };
 
 /********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
-
+/* Sze Yan Li
+ * ALTERNATING BIT VERSION
+ * CIS432
 /*********/
 /* Global Variables for A & B*/
 int NAK = 0;
@@ -48,20 +53,65 @@ int currSeq; //current sequence number
 int isBusy; //boolean if A is still working on msg with B, 0 = false
 struct pkt currPkt; //the current packet 
 float sampleRTT;
+float tmpTime; //saves time when packet was sent from A to B
 float estimatedRTT;
 float devRTT;
 float timeoutInterval;
 
 /* Global Variables for B*/
 int expectedSeq; //what B expects for a seq number in arriving packet
+struct pkt rcvPkt; //packet used to sending back ACK/NACK, chcksum to A
 
 /* called from layer 5, passed the data to be sent to other side */
 A_output(message)
 struct msg message;
 {
+    if (TRACE == 2) {
+        printf("    ** 'A' receives call from layer5\n");
+    }
+
     //only send if there's no outstanding, unacked msg to B
     if (!isBusy) {
+        if (TRACE == 2) {
+            printf("    ** 'A' is not busy and accepts the msg\n");
+        }
 
+        //A is now busy trying to sending a msg to B
+        isBusy = 1;
+
+        //set seq num
+        currPkt.seqnum = currSeq;
+        //set next seq num
+        if (currSeq == 0) {
+            currSeq = 1;
+        } else {
+            currSeq = 0;
+        }
+
+        //place message into payload
+        int i;
+        for (i = 0; i < 20; i++) {
+            currPkt.payload[i] = message.data[i];
+        }
+
+        //compute and set checksum
+        currPkt.checksum = calc_checksum(currPkt);
+
+        //send pkt
+        if (TRACE == 2) {
+            printf("    ** 'A' sends made packet to layer3\n");
+        }
+        tolayer3(0, currPkt);
+
+        //save current time (used for calculating timeoutInterval later)
+        tmpTime = simtime;
+
+        //start timer
+        starttimer(0, timeoutInterval);
+    } else {
+        if (TRACE == 2) {
+            printf("    -- 'A' is busy, so msg is discarded\n");
+        }
     }
 
 }
@@ -87,12 +137,55 @@ struct msg message;
 A_input(packet)
 struct pkt packet;
 {
+    if (TRACE == 2) {
+        printf("    ** 'A' receives packet from layer3\n");
+    }
 
+    //check the packet's checksum against currPkt's checksum
+    int pktNotCorrupt = packet.checksum == calc_checksum(packet);
+    int pktCorrectChksum = packet.checksum == currPkt.checksum;
+    if (pktNotCorrupt && pktCorrectChksum) {
+        if (TRACE == 2) {
+            printf("    ** packet received is not corrupted\n");
+        }
+
+        if (packet.acknum == ACK) {
+            //stop the timer
+            stoptimer(0);
+
+            //'A' is no longer busy since it's done working on the packet
+            isBusy = 0;
+
+            //get and calculate the timeoutinterval
+            sampleRTT = simtime - tmpTime;
+            A_set_timeout();
+
+            if (TRACE == 2) {
+                printf("    ** packet contains an ACK. 'A' is no longer busy\n");
+            }
+        } else if (packet.acknum == NAK) {
+            if (TRACE == 2) {
+                printf("    -- packet contains a NAK. Wait for retransmission\n");
+            }
+        }
+    } else {
+        if (TRACE == 2) {
+            printf("    -- packet received is corrupted\n");
+        }
+    }
 }
 
 /* called when A's timer goes off */
 A_timerinterrupt() {
+    //send pkt
+    if (TRACE == 2) {
+        printf("    -- 'A' timed out\n");
+        printf("    -- 'A' resending packet to layer3\n");
+    }
+    tolayer3(0, currPkt);
 
+    //start timer
+    starttimer(0, timeoutInterval);
 }
 
 /* the following routine will be called once (only) before any other */
@@ -106,11 +199,12 @@ A_init() {
     currPkt.acknum = 0;
     currPkt.checksum = 0;
     int i;
-    for (i = 0; i <= 20; i++) {
+    for (i = 0; i < 20; i++) {
         currPkt.payload[i] = 0;
     }
     //init timer variables
     sampleRTT = 0;
+    tmpTime = 0;
     estimatedRTT = 0;
     devRTT = 0;
     timeoutInterval = 20;
@@ -123,6 +217,61 @@ A_init() {
 B_input(packet)
 struct pkt packet;
 {
+    if (TRACE == 2) {
+        printf("    ** 'B' receives packet from layer3\n");
+    }
+    int tmpChecksum = calc_checksum(packet);
+    if (packet.checksum == tmpChecksum) {
+        if (TRACE == 2) {
+            printf("    ** packet received is not corrupted\n");
+        }
+
+        //check expected seqnum aginst seqnum in packet
+        if (expectedSeq == packet.seqnum) {
+            //send ack with checksum
+            rcvPkt.checksum = packet.checksum;
+            rcvPkt.acknum = ACK;
+            if (TRACE == 2) {
+                printf("    ** 'B' sending ACK to layer3\n");
+            }
+            tolayer3(1, rcvPkt);
+
+            //send message to layer5 this good packet           
+            if (TRACE == 2) {
+                printf("    ** 'B' sending msg to layer5\n");
+            }
+            tolayer5(1, packet.payload);
+
+            //update expected seq number
+            if (expectedSeq == 0) {
+                expectedSeq = 1;
+            } else {
+                expectedSeq = 0;
+            }
+        } else {
+            //maybe B's previous ack was lost
+            //send ack with checksum
+            rcvPkt.checksum = packet.checksum;
+            rcvPkt.acknum = ACK;
+            if (TRACE == 2) {
+                printf("    *- 'B' resending ACK to layer3\n");
+            }
+            tolayer3(1, rcvPkt);
+        }
+
+    } else {
+        if (TRACE == 2) {
+            printf("    -- packet received is corrupted\n");
+
+            //send nack with checksum
+            rcvPkt.checksum = packet.checksum;
+            rcvPkt.acknum = NAK;
+            if (TRACE == 2) {
+                printf("    *- 'B' sending NAK to layer3\n");
+            }
+            tolayer3(1, rcvPkt);
+        }
+    }
 }
 
 /* called when B's timer goes off */
@@ -133,6 +282,33 @@ B_timerinterrupt() {
 
 /* entity B routines are called. You can use it to do any initialization */
 B_init() {
+    //init variables
+    expectedSeq = 0;
+
+    //init standard return-to-A packet
+    rcvPkt.seqnum = 0;
+    rcvPkt.acknum = 0;
+    rcvPkt.checksum = 0;
+    int i;
+    for (i = 0; i < 20; i++) {
+        rcvPkt.payload[i] = 0;
+    }
+}
+
+/* Helper method to calculate the checksum*/
+int calc_checksum(const struct pkt packet) {
+    int checksum = 0;
+
+    checksum += packet.seqnum;
+    checksum += packet.acknum;
+
+    //convert chara into int and continue calculating checksum
+    int i;
+    for (i = 0; i < 20; i++) {
+        checksum += (int) packet.payload[i];
+    }
+
+    return checksum;
 }
 
 /*****************************************************************
@@ -173,10 +349,9 @@ struct event *evlist = NULL; /* the event list */
 
 
 
-int TRACE = 1; /* for my debugging */
+
 int nsim = 0; /* number of messages from 5 to 4 so far */
 int nsimmax = 0; /* number of msgs to generate, then stop */
-float time = 0.000;
 float lossprob; /* probability that a packet is dropped  */
 float corruptprob; /* probability that one bit is packet is flipped */
 float lambda; /* arrival rate of messages from layer 5 */
@@ -214,7 +389,7 @@ main() {
                 printf(", fromlayer3 ");
             printf(" entity: %d\n", eventptr->eventity);
         }
-        time = eventptr->evtime; /* update time to next event time */
+        simtime = eventptr->evtime; /* update time to next event time */
         if (nsim == nsimmax)
             break; /* all done with simulation */
         if (eventptr->evtype == FROM_LAYER5) {
@@ -257,7 +432,7 @@ main() {
     }
 
 terminate:
-    printf(" Simulator terminated at time %f\n after sending %d msgs from layer5\n", time, nsim);
+    printf(" Simulator terminated at time %f\n after sending %d msgs from layer5\n", simtime, nsim);
 }
 
 init() /* initialize the simulator */ {
@@ -294,7 +469,7 @@ init() /* initialize the simulator */ {
     nlost = 0;
     ncorrupt = 0;
 
-    time = 0.0; /* initialize time to 0.0 */
+    simtime = 0.0; /* initialize time to 0.0 */
     generate_next_arrival(); /* initialize event list */
 }
 
@@ -329,7 +504,7 @@ generate_next_arrival() {
     x = lambda * jimsrand()*2; /* x is uniform on [0,2*lambda] */
     /* having mean of lambda        */
     evptr = (struct event *) malloc(sizeof (struct event));
-    evptr->evtime = time + x;
+    evptr->evtime = simtime + x;
     evptr->evtype = FROM_LAYER5;
     if (BIDIRECTIONAL && (jimsrand() > 0.5))
         evptr->eventity = B;
@@ -344,7 +519,7 @@ struct event *p;
     struct event *q, *qold;
 
     if (TRACE > 2) {
-        printf("            INSERTEVENT: time is %lf\n", time);
+        printf("            INSERTEVENT: time is %lf\n", simtime);
         printf("            INSERTEVENT: future time will be %lf\n", p->evtime);
     }
     q = evlist; /* q points to header of list in which p struct inserted */
@@ -394,7 +569,7 @@ int AorB; /* A or B is trying to stop timer */
     struct event *q, *qold;
 
     if (TRACE > 2)
-        printf("          STOP TIMER: stopping timer at %f\n", time);
+        printf("          STOP TIMER: stopping timer at %f\n", simtime);
     /* for (q=evlist; q!=NULL && q->next!=NULL; q = q->next)  */
     for (q = evlist; q != NULL; q = q->next)
         if ((q->evtype == TIMER_INTERRUPT && q->eventity == AorB)) {
@@ -426,7 +601,7 @@ float increment;
     char *malloc();
 
     if (TRACE > 2)
-        printf("          START TIMER: starting timer at %f\n", time);
+        printf("          START TIMER: starting timer at %f\n", simtime);
     /* be nice: check to see if timer is already started, if so, then  warn */
     /* for (q=evlist; q!=NULL && q->next!=NULL; q = q->next)  */
     for (q = evlist; q != NULL; q = q->next)
@@ -437,7 +612,7 @@ float increment;
 
     /* create future event for when timer goes off */
     evptr = (struct event *) malloc(sizeof (struct event));
-    evptr->evtime = time + increment;
+    evptr->evtime = simtime + increment;
     evptr->evtype = TIMER_INTERRUPT;
     evptr->eventity = AorB;
     insertevent(evptr);
@@ -490,7 +665,7 @@ struct pkt packet;
        medium can not reorder, so make sure packet arrives between 1 and 10
        time units after the latest arrival time of packets
        currently in the medium on their way to the destination */
-    lastime = time;
+    lastime = simtime;
     /* for (q=evlist; q!=NULL && q->next!=NULL; q = q->next) */
     for (q = evlist; q != NULL; q = q->next)
         if ((q->evtype == FROM_LAYER3 && q->eventity == evptr->eventity))
